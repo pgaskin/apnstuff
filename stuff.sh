1914b6960a0d9a997f7c3b4dddaf95c97da61a1c
Patrick Gaskin <patrick@pgaskin.net>
2024-10-23 03:32:03 -0400

    more stuff

--- stuff.sh
+++ stuff.sh
@@ -2,13 +2,22 @@
 set -euxo pipefail
 
 for x in carrier_list carrier_settings carrierId; do
-for y in android-13.0.0_r3 android14-qpr3-release; do
+for y in android-12.0.0_r4 android-13.0.0_r3 android14-qpr3-release; do
 protoc -I $y $x.proto -o $y/$x.proto.pb
 done
 done
 
 rm -rf out
 mkdir out
+
+mkdir out/2021
+bsdtar xOf raven_SD1A.210817.015.A4/TelephonyProvider.apk assets/carrier_list.pb | protoscope -descriptor-set android-12.0.0_r4/carrierId.proto.pb -message-type carrierIdentification.CarrierList -print-field-names -print-enum-names > out/2021/carrierId.txt
+protoscope -descriptor-set android-12.0.0_r4/carrier_list.proto.pb -message-type com.google.carrier.CarrierList -print-field-names -print-enum-names < raven_SD1A.210817.015.A4/CarrierSettings/carrier_list.pb > out/2021/carrier_list.txt
+for x in freedommobile bell telus shaw rogers videotron; do
+protoscope -descriptor-set android-12.0.0_r4/carrier_settings.proto.pb -message-type com.google.carrier.CarrierSettings -print-field-names -print-enum-names < raven_SD1A.210817.015.A4/CarrierSettings/${x}_ca.pb > out/2021/carrier_settings_$x.txt
+done
+python3 lineage/scripts/carriersettings-extractor/carriersettings_extractor.py -i raven_SD1A.210817.015.A4/CarrierSettings -a out/2021 -v out/2021
+xmllint --format out/2021/apns-conf.xml --output out/2021/apns-conf.xml
 
 mkdir out/2022
 bsdtar xOf panther_TD1A.220804.009.A2/TelephonyProvider.apk assets/carrier_list.pb | protoscope -descriptor-set android-13.0.0_r3/carrierId.proto.pb -message-type carrierIdentification.CarrierList -print-field-names -print-enum-names > out/2022/carrierId.txt
